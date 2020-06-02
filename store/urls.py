c0dc4ceba6143e99ede14f8734a94e355face863
Prosvetx <Prosvetx@github.com>
2020-06-02 20:52:52 +0400

    checkout logic

--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+    #path('b', views.b, name="b"),
 
     path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="update_item")
 
 ]
 
